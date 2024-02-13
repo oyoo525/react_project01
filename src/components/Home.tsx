981765109aba4229a5fd379afd98033f0c935980
oyoo525 <simsh007@naver.com>
2024-02-13 18:32:34 +0900

    상단 광고배너 marquee 적용

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,106 +1,36 @@
 import React from "react";
-import img01 from '../assets/img/다운로드.png';
+import img01 from '../assets/img/bg01.png';
 import { Link } from "react-router-dom";
-import Marquee from 'react-marquee-slider';
+import '../assets/css/home.css';
 
 export default function Home() {
 	return (
-		<div style={{
-			width : "100vw",
-			justifyContent : "center",
-			textAlign : "center",
-			padding : "5vw",
-			backgroundColor : "#DDEAF3"
-		}}>
+		<div className="whole">
 			<div>
-				<span style={{
-					fontSize : "11vw",
-					fontWeight : "bold",
-					textAlign : "center"
-				}}>SITE OF THE DAY</span>
+				<span className="title">SITE OF THE DAY</span>
 			</div>
 			<div>
-				<span style={{
-					fontSize : "2.5vw",
-					color : "gray"
-				}}>- Site of the Day - 7.95 - Feb 6, 2024 - Active Theory V6</span>
+				<span className="sub-title">- Site of the Day - 7.95 - Feb 6, 2024 - Active Theory V6</span>
 			</div>
-			<div className="user_wrap" style={{
-				position : "relative",
-				width : "90vw",
-				height : "50vw",
-				overflow : "hidden",
-				borderRadius : "15px",
-				margin : "auto",
-				marginTop : "4vw",
-				marginBottom : "9vw",
-			}}>
-				<div className="user_img">
-					<Link to="">
-						<img src={img01} style={{
-							width : "100%"
-						}}/>
-					</Link>
+			<div className="user-wrap">
+				<div className="user-img">
+					<img src={img01} className="back-img"/>
 				</div>
-				<div className="user_carousel" style={{
-					position : "absolute",
-					zIndex : 8,
-					top : 0,
-					bottom : 0,
-					width : "100%",
-					display : "flex",
-					justifyContent : "center",
-					alignItems : "center"
-				}}>
-
+				<div className="user-carousel">
 				</div>
-				<div className="user_btn" style={{
-					position : "absolute",
-					zIndex : 10,
-					top : 0,
-					bottom : 0,
-					width : "100%",
-					display : "flex",
-					justifyContent : "center",
-					alignItems : "center"
-				}}>
-					<div className="btn-border" style={{
-						border : "3px solid gray",
-						borderRadius : "10px",
-						width : "20%",
-						height : "10%",
-						display : "flex",
-						justifyContent : "center",
-						alignItems : "center"
-					}}>
-						<span style={{
-							fontSize : "2vw",
-							fontWeight : "bold",
-							color : "gray",
-						}}>Explore SOTD</span>
+				<Link to="">
+					<div className="user-btn">
+						<div className="btn-border">
+							<span className="btn-name">Explore SOTD</span>
+						</div>
 					</div>
-				</div>
+				</Link>
 			</div>
 			<div>
-				<Link to="" style={{
-				display : "flex",
-				justifyContent : "center",
-				textDecoration : "none",
-				color : "black"
-				}}>
-					<i className="bi bi-arrow-up-right-square-fill" style={{
-						fontSize : "24px"
-					}}></i>
+				<Link to="" className="under-title">
+					<i className="bi bi-arrow-up-right-square-fill"></i>
 					<div>
-						<strong style={{
-							textDecorationLine : "underline",
-							color : "pink",
-							
-						}}><span style={{
-							color : "black",
-							fontSize : "24px",
-							margin : "0 4px"
-						}}>Active Theory</span></strong>
+						<strong className="underline"><span className="under-title-text">Active Theory</span></strong>
 					</div>
 					<div>
 						<span >pro</span>
--- src/components/temp/Advertising.tsx
+++ src/components/temp/Advertising.tsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../../assets/css/advertising.css';
 
 
 export default function Advertising() {
 	return(
-		<div
-			style={{
-				width : "100%",
-				height : "50px",
-				backgroundColor : "black"
-			}}>
-			<Link to="" style={{
-				textDecoration : "none"
-			}}>
-				<div>
-					<span style={{
-					fontSize : "17px",
-					fontWeight : "bold",
-					color : "white",
-					lineHeight : "50px"
-					}}><i className="bi bi-lightning-fill"></i>광고를 입력하세요.</span>
+		<div className="container-a">
+			<Link to="" className="link-text">
+				<div className="track">
+					<span className="adver-title">
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+						<i className="bi bi-lightning-fill"></i>광고를 입력하세요.&nbsp;
+					</span>
 				</div>
 			</Link>
 		</div>
